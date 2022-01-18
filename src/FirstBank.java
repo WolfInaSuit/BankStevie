8eef722ae63695d4459689aae4eb53667983cf54
WolfInaSuit <Stevferro1@gmail.com>
2022-01-18 07:43:12 -0500

    Initial Commit

---
+++ src/FirstBank.java
@@ -0,0 +1,8 @@
+public class FirstBank {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account stevie = new Account("Stevie Ferro", "A00001");
+        stevie.showMenu();
+    }
+}
